2ce9ae94de411626df465989919cb7a53688b9a7
Aditya Abhishek <encarcio95@gmail.com>
2020-04-23 15:37:22 +0530

    sixth-commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String home() {
-      return "index";
+      return "index.html";
     }
 
     public static void main(String[] args) {

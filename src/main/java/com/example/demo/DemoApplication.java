64975d5c66dfbd0af462e5109bea705371c2aa2e
Aditya Abhishek <encarcio95@gmail.com>
2020-04-24 11:39:22 +0530

    final-commit-1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
 
-@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    public String home() {
-      return "index.html";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
 }
---
+++ src/main/java/com/example/demo/inputData.java
@@ -0,0 +1,101 @@
+package com.example.demo;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+
+public class inputData {
+ private  String genre;
+	private String exp;
+	private String stream;
+	Map<String,String> out=new HashMap<String,String>();	
+public Map<String, String> getOut() {
+		return out;
+	}
+
+
+	public void setOut(Map<String, String> out) {
+		this.out = out;
+	}
+
+
+public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+		System.out.println("DATA IN THE INPUT DATA CLASS IS "+this.genre );
+	}
+
+
+	public String getExp() {
+		return exp;
+	}
+
+
+	public void setExp(String exp) {
+		this.exp = exp;
+		System.out.println("DATA IN THE INPUT DATA CLASS IS "+this.exp );
+	}
+
+
+	public String getStream() {
+		return stream;
+	}
+
+
+	public void setStream(String stream) {
+		this.stream = stream;
+		System.out.println("DATA IN THE INPUT DATA CLASS IS "+this.stream );
+		putinfo();
+	}
+
+
+public void putinfo()
+{  
+	
+	String gen="%"+genre+"%";
+	String st=stream.substring(0,4)+"%";
+	Integer exper=new Integer(exp);
+	System.out.println("DATA IN THE INPUT DATA CLASS IS "+gen+" "+st+""+exper);
+	
+	String str="select * from anime where genre LIKE ? and exp=? and stream LIKE ?";
+	
+	 try (
+             Connection conn = DriverManager.getConnection(
+				"jdbc:postgresql://localhost:5432/postgres",
+				"postgres", "xxxx");
+             
+               Statement stmt = conn.createStatement();
+             ){
+		 
+		 PreparedStatement pre=conn.prepareStatement(str); 
+		 pre.setString(1, gen);
+		 pre.setInt(2, exper);
+		 pre.setString(3, st);
+		ResultSet se= pre.executeQuery();
+		while(se.next())
+		{   String s1=se.getString(2);
+		     String s2=se.getString(6);
+		     out.put(s1, s2);
+			System.out.println(s1);
+			System.out.println(s2);
+		}
+	
+	
+	
+	
+	 }
+	 catch (Exception e)
+	 {
+		 e.printStackTrace();
+	 }
+	
+}
+}

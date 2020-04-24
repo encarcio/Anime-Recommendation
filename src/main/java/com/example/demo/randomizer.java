9da102bf233fc57da807a5a16b10b981826f49fe
Aditya Abhishek <encarcio95@gmail.com>
2020-04-24 13:17:37 +0530

    final-commit-8

--- src/main/java/com/example/demo/randomizer.java
+++ src/main/java/com/example/demo/randomizer.java
@@ -27,7 +27,7 @@ public class randomizer {
     }
 
     private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("postgres://rlrbwnwrimhkrf:2b9d9f64a5bdeb6329c0dc378664bb5562991623386aa9b6bd45d8d447fa757d@ec2-50-17-21-170.compute-1.amazonaws.com:5432/dlg747a1gkdl8"));
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
